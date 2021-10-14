57e5217d341d06e53ee4221a40c1e9ff12ac3dae
TumbaevAA <theonetommygun1@gmail.com>
2021-10-14 10:38:32 +0700

    added dynamic tests for point and line

--- prog_lab2.cpp
+++ prog_lab2.cpp
@@ -12,7 +12,7 @@ int main()
 {
     //Тест Point
     Point firstPoint, secondPoint;
-    printf("Enter the coordinates of the first point: ");
+    printf("Point test\nEnter the coordinates of the first point: ");
     pointInput(&firstPoint);
     printf("\nEnter the coordinates of the second point: ");
     pointInput(&secondPoint);
@@ -25,27 +25,59 @@ int main()
     printf("\nDistance between points: %f", pointDistance(firstPoint, secondPoint));
 
 
+
+    //Тест динамических Point
+    Point* firstDynamicPoint = (Point*)malloc(sizeof(Point)) , * secondDynamicPoint = (Point*)malloc(sizeof(Point));
+    printf("\n\n\nDynamic Point test\nEnter the coordinates of the first dynamic point: ");
+    pointInput(firstDynamicPoint);
+    printf("\nEnter the coordinates of the second dynamic point: ");
+    pointInput(secondDynamicPoint);
+
+    printf("\nThe coordinates of the first dynamic point: ");
+    pointOutput(*firstDynamicPoint);
+    printf("\nThe coordinates of the second dynamic point: ");
+    pointOutput(*secondDynamicPoint);
+
+    printf("\nDistance between dynamic points: %f", pointDistance(*firstDynamicPoint, *secondDynamicPoint));
+
+    free(firstDynamicPoint);
+    free(secondDynamicPoint);
+
+
+
+
+
     //Тест Line
     Line line;
-    printf("\n\n\nEnter the coordinates of the start and the end of the line: ");
+    printf("\n\n\nLine test\nEnter the coordinates of the start and the end of the line: ");
     lineInput(&line);
     lineOutput(line);
 
+
+    //Тест динамической Line
+    Line* dynamicLine = (Line*)malloc(sizeof(Line));
+    printf("\n\n\nDynamic line test\nEnter the coordinates of the start and the end of the dynamic line: ");
+    lineInput(dynamicLine);
+    lineOutput(*dynamicLine);
+    free(dynamicLine);
+
+
+
     //Тест Triangle
     Triangle triangle;
-    printf("\n\n\nEnter the coordinates of the first, the second and the third points: ");
+    printf("\n\n\nTriangle test\nEnter the coordinates of the first, the second and the third points: ");
     triangleInput(&triangle);
     triangleOutput(triangle);
 
     //Тест Square
     Square square;
-    printf("\n\n\nEnter the coordinates of the first, the second, the third and the fourth points: ");
+    printf("\n\n\nSquare test\nEnter the coordinates of the first, the second, the third and the fourth points: ");
     squareInput(&square);
     squareOutput(square);
 
     //Тест Circle
     Circle circle;
-    printf("\n\n\nEnter the coordinates of the center and radius: ");
+    printf("\n\n\nCircle test\nEnter the coordinates of the center and radius: ");
     circleInput(&circle);
     circleOutput(circle);
     
